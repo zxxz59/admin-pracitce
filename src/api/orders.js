dcffd7d91ded597cba90517abad4589000338b48
zxxz59 <zx59mail@163.com>
2022-07-20 11:09:06 +0800

    订单列表完成

--- src/api/orders.js
+++ src/api/orders.js
@@ -1,8 +1,22 @@
 import request from '@/utils/request'
 export function getOrdersListAPI(params) {
   return request({
-    method: 'orders',
-    url: 'menus',
+    method: 'get',
+    url: 'orders',
     params
   })
 }
+// consignee_addr: ""
+// create_time: 1658147663
+// is_send: "否"
+// order_fapiao_company: ""
+// order_fapiao_content: ""
+// order_fapiao_title: "个人"
+// order_id: 67
+// order_number: "itcast-g7kmck71vjaujfgoi"
+// order_pay: "0"
+// order_price: 203
+// pay_status: "0"
+// trade_no: ""
+// update_time: 1658147663
+// user_id: 1
--- src/views/Order/index.vue
+++ src/views/Order/index.vue
@@ -10,10 +10,12 @@
             v-model="searchText"
             @keyup.enter.native="searchBtn"
           >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="searchBtn"
+            ></el-button>
           </el-input>
-        </el-col>
-        <el-col :span="6">
-          <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
       <!-- #endregion -->
@@ -27,25 +29,32 @@
           :row-class-name="tableRowClassName"
         >
           <el-table-column type="index" label="#" width="50"> </el-table-column>
-          <el-table-column prop="username" label="姓名" width="180">
+          <el-table-column prop="order_number" label="订单编号">
           </el-table-column>
-          <el-table-column prop="mobile" label="电话"> </el-table-column>
-          <el-table-column prop="role_name" label="角色"> </el-table-column>
-          <el-table-column prop="mg_state" label="状态" width="150">
+          <el-table-column prop="order_price" label="订单价格">
+          </el-table-column>
+          <el-table-column prop="pay_status" label="是否付款">
             <template v-slot="{ row }">
-              <el-switch
-                v-model="row.mg_state"
-                active-color="#13ce66"
-                inactive-color="#ff4949"
-                @change="switchChange(row.id, row.mg_state)"
-              >
-              </el-switch>
+              <el-tag type="danger" effect="dark" v-if="row.pay_status === '0'">
+                未支付
+              </el-tag>
+              <el-tag type="success" effect="dark" v-else> 已支付 </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="is_send" label="是否发货"> </el-table-column>
+          <el-table-column prop="create_time" label="下单时间">
+            <template v-slot="{ row }">
+              {{ row.create_time | dealTime }}
             </template>
           </el-table-column>
           <el-table-column label="操作">
             <template v-slot="{ row }">
-              <el-button type="primary" icon="el-icon-edit" circle>
-                {{ row.id }}
+              <el-button
+                type="primary"
+                icon="el-icon-edit"
+                @click="editAddress(row.order_id)"
+              >
+                修改收获地址
               </el-button>
             </template>
           </el-table-column>
@@ -67,15 +76,32 @@
       </div>
       <!-- #endregion -->
     </my-card>
+    <!-- #region === 弹出对话框 -->
+    <el-dialog
+      title="修改地址"
+      :visible.sync="dialogVisible"
+      width="30%"
+      center
+    >
+      <el-cascader v-model="value" :options="options"></el-cascader>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="okBtn"> 确 定 </el-button>
+      </span>
+    </el-dialog>
+    <!-- #endregion -->
   </div>
 </template>
 
 <script>
+import { getOrdersListAPI } from '@/api/orders'
+import city from '@/utils/city_element'
 export default {
   name: 'OrderIndex',
   components: {},
   data() {
     return {
+      dialogVisible: false,
       loading: false,
       searchText: '',
       orders: [],
@@ -83,13 +109,37 @@ export default {
         pagenum: 1,
         pagesize: 5
       },
-      total: null // 总数量
+      total: null, // 总数量
+      options: [],
+      value: []
     }
   },
-  created() {},
+  created() {
+    this.getOrdersList()
+  },
   methods: {
-    async getOrdersList() {},
-    async searchBtn() {},
+    tableRowClassName({ row, rowIndex }) {
+      if (rowIndex % 2 === 0) {
+        return 'warning-row'
+      }
+      return ''
+    },
+    async getOrdersList() {
+      try {
+        const data = await getOrdersListAPI({
+          ...this.page,
+          query: this.searchText
+        })
+        this.page.pagenum = data.pagenum - 0
+        this.total = data.total
+        this.orders = data.goods
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    searchBtn() {
+      this.$message.error('服务器繁忙，稍后重试')
+    },
     handleSizeChange(val) {
       this.page.pagesize = val
       this.getOrdersList()
@@ -97,6 +147,14 @@ export default {
     handleCurrentChange(val) {
       this.page.pagenum = val
       this.getOrdersList()
+    },
+    editAddress() {
+      this.dialogVisible = true
+      this.options = city
+    },
+    okBtn() {
+      this.dialogVisible = false
+      this.$message.warning('该地区暂时无法发货')
     }
   },
   computed: {},
@@ -111,5 +169,8 @@ export default {
 
 :deep(.el-table .success-row) {
   background: #f0f9eb;
+}
+:deep(.el-table) {
+  font-size: 14px;
 }
 </style>
