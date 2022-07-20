810583721811945eae55dc1361d762f7dda8aa0b
zxxz59 <zx59mail@163.com>
2022-07-20 08:32:07 +0800

    404

---
+++ src/views/Order/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {},
+  computed: {},
+  watch: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
